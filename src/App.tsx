bef6d4db1c965722b9b4d0c3a7558a7d9a4e0c00
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-07-25 11:20:24 +0330

    create a list component and use generics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Counter from './components/Counter';
 import Heading from './components/Heading';
+import List from './components/List';
 import Section from './components/Section';
 import { useState } from 'react';
 function App() {
@@ -9,6 +10,10 @@ function App() {
             <Heading title={'hello!'} />
             <Section title={'different title'}>This is my section</Section>
             <Counter setCount={setCount}>count is {count}</Counter>
+            <List
+                items={['coffe', 'game', 'code']}
+                render={(item: string) => <span className='gold'>{item}</span>}
+            />
         </>
     );
 }

ea902d9d6fbc84118cf2a949a6ac03d6bd7751eb
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-07-25 11:05:10 +0330

    create a counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import Counter from './components/Counter';
 import Heading from './components/Heading';
 import Section from './components/Section';
-
+import { useState } from 'react';
 function App() {
+    const [count, setCount] = useState<number>(0);
     return (
         <>
             <Heading title={'hello!'} />
             <Section title={'different title'}>This is my section</Section>
+            <Counter setCount={setCount}>count is {count}</Counter>
         </>
     );
 }

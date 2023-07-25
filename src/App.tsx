a00efa52344ef06456553e3b780724bc97a8e333
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-07-25 13:07:25 +0330

    practice with useEffect useState useMemo useCallback useRef hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+    useCallback,
+    useState,
+    MouseEvent,
+    KeyboardEvent,
+    useMemo,
+    useRef,
+    useEffect,
+} from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface User {
+    id: number;
+    username: string;
 }
 
-export default App
+type fibFunc = (n: number) => number;
+
+const fib: fibFunc = n => {
+    if (n < 2) return n;
+    return fib(n - 1) + fib(n - 2);
+};
+
+const myNumber: number = 37;
+
+function App() {
+    const [count, setCount] = useState<number>(0);
+    const [users, setUsers] = useState<User[] | null>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        console.log('mounting');
+        console.log('users', users);
+        return () => console.log('unmounting');
+    }, [users]);
+
+    const addTwo = useCallback(
+        (): // e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>
+        void => setCount(prev => prev + 2),
+        []
+    );
+
+    const result = useMemo<number>(() => fib(myNumber), [myNumber]);
+
+    return (
+        <>
+            <h1>{count}</h1>
+            <button onClick={addTwo}>Add two</button>
+            <h2>{result}</h2>
+            <input ref={inputRef} type='text' />
+        </>
+    );
+}
+
+export default App;

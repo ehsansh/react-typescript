612f1d9d3cd8d9906682bf04e289f41d19175a1d
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-07-25 13:56:01 +0330

    create a counter

---
+++ src/Counter.tsx
@@ -0,0 +1,22 @@
+import { ReactNode, useState } from 'react';
+
+type ChildrenType = {
+    children: (num: number) => ReactNode;
+};
+
+export const Counter = ({ children }: ChildrenType) => {
+    const [count, setCount] = useState<number>(1);
+
+    const increment = () => setCount(prev => prev + 1);
+    const decrement = () => setCount(prev => prev - 1);
+
+    return (
+        <>
+            <h1> {children(count)} </h1>
+            <div>
+                <button onClick={increment}>+</button>
+                <button onClick={decrement}>-</button>
+            </div>
+        </>
+    );
+};
